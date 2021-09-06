e9de7f474c815acff31c3dea4beb3164c2be7a99
JIWON NA <johnny.na@groundx.xyz>
2021-09-06 13:58:58 +0900

    SPM

--- Package.swift
+++ Package.swift
@@ -12,17 +12,15 @@ let package = Package(
             targets: ["HDWallet"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.4.1"),
+        .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift", from: "0.1.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HDWallet",
-            dependencies: []),
-        .testTarget(
-            name: "HDWalletTests",
-            dependencies: ["HDWallet"]),
+            dependencies: ["CryptoSwift", "secp256k1"],
+            path:"HDWalletKit"),
     ]
 )
